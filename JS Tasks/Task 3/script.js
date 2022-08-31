e14541fec3ba3dd476de2a563276932a6d2527e5
ZydBra <z.brazionis@gmail.com>
2022-08-31 21:57:59 +0300

    Atlikta 3 užduotis

--- JS Tasks/Task 3/script.js
+++ JS Tasks/Task 3/script.js
@@ -12,3 +12,32 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+function getData () {
+    fetch(ENDPOINT)
+        .then(res => res.json())
+        .then(data => {
+            ShowUsers(data)
+        })
+        .catch(error => {
+            alert(error)
+        })
+};
+
+function ShowUsers(data) {
+    document.getElementById('message').remove();
+    data.forEach(entry => {
+        const outputDiv = document.getElementById('output');
+        const userDiv = document.createElement('div');
+        const userLogin = document.createElement('h3');
+        const userUrl = document.createElement('img');
+        outputDiv.append(userDiv);
+        userDiv.className = "user_div";
+        userLogin.textContent = entry.login;
+        userUrl.src = entry.avatar_url;
+        userUrl.alt = "User " + entry.login + " picture"; 
+        userDiv.append(userLogin, userUrl)
+    });
+}
+
+document.getElementById('btn').addEventListener('click', getData);
