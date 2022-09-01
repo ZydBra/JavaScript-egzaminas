580ecd14954a27dd3bc8d0b46421edc02ed1ba5d
ZydBra <z.brazionis@gmail.com>
2022-09-01 21:12:14 +0300

    Atlikta 8 užduotis

--- JS Tasks/Task 8/script.js
+++ JS Tasks/Task 8/script.js
@@ -5,3 +5,44 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+// 1 variantas susikuria objektas kartu su atliktai matematiniais veiksmais:
+function Calculator(a, b) {
+    this.a = a;
+    this.b = b;
+    this.sum = this.a + this.b;
+    this.subtraction = this.a - this.b;
+    this.multiplication = this.a * this.b;
+    this.division = this.a / this.b;
+};
+
+const calculator1 = new Calculator(2, 2);
+const calculator2 = new Calculator(6, 3);
+
+console.log(calculator1, calculator2);
+
+// 2 variantas su metodais:
+function Calculator2(a, b) {
+    this.a = a;
+    this.b = b;
+
+    this.sum = function () {
+        return this.a + this.b
+    }
+
+    this.subtraction = function () {
+        return this.a - this.b
+    }
+
+    this.multiplication = function () {
+        return this.a * this.b
+    }
+
+    this.division = function () {
+        return this.a / this.b
+    }
+};
+
+const calculator3 = new Calculator2(2, 2);
+
+console.log(calculator3.sum(), calculator3.subtraction(), calculator3.multiplication(), calculator3.division());
